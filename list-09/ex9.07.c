62a8e265e005c1a53143c4153dd780ab282f3dcd
Vinicius Naziozeno Santoro do Rio <83036509+Vinicius-Santoro@users.noreply.github.com>
2021-09-17 22:23:43 -0300

    9.characters and strings

--- list-09/ex9.07.c
+++ list-09/ex9.07.c
@@ -1,7 +1,7 @@
 /*
     Nome: ex.9.7.c
     Autor: Vinicius Naziozeno Santoro do Rio.
-    Descrição: Capítulo 9, exercício 7.
+    DescriÃ§Ã£o: CapÃ­tulo 9, exercÃ­cio 7.
 */
     #include <stdio.h>
     #include <stdlib.h>
